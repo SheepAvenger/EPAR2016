b5bcb96b6ee6957e9a229bbc69ffc87be73ec09a
SheepAvenger <robert.mccook.elm@gmail.com>
2017-02-23 14:44:00 -0800

    test

--- app/src/main/java/com/softwarei/epar2016/MainMenu.java
+++ app/src/main/java/com/softwarei/epar2016/MainMenu.java
@@ -1,5 +1,7 @@
 package com.softwarei.epar2016;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,17 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+         //2130837587
+        Bitmap t[] = new Bitmap[2];
+        int x;
+        for(int i = 0; i <= 1; i++)
+        {
+            x = R.drawable.l0;
+            x +=i;
+            t[i] = BitmapFactory.decodeResource(this.getResources(), x);
+            System.out.println(t[i]);
+
+        }
         setContentView(R.layout.activity_main_menu);
     }
 }
