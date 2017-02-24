67907c416c4b3021df76f8d05dd0ade2a3aa46c9
SheepAvenger <robert.mccook.elm@gmail.com>
2017-02-23 21:36:46 -0800

    Model Finished and working

--- app/src/main/java/com/softwarei/epar2016/Scores.java
+++ app/src/main/java/com/softwarei/epar2016/Scores.java
@@ -3,9 +3,13 @@ package com.softwarei.epar2016;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.lang.String;
 
 /**
@@ -17,38 +21,83 @@ public class Scores extends AppCompatActivity
     private highScore scores[];
     private String scoreString[];
     private Context ctx;
+    String FILE;
 
     public Scores(Context current)
     {
         int i = 0;
+        FILE = "scores.txt";
         this.ctx = current;
         scores = new highScore[11];
         scoreString = new String[10];
-        InputStream is = ctx.getResources().openRawResource(R.raw.scores);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-        if (is!=null)
-        {
-            try
+        //InputStream is = ctx.getResources().openRawResource(R.raw.scores);
+          try
             {
-                while (i != 10)
+                FileInputStream is = ctx.openFileInput(FILE);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                if(is != null)
                 {
-                    scoreString[i] = reader.readLine();
-                    highScore s = new highScore();
-                    String parse[] = scoreString[i].split(" ");
-                    s.name = parse[0];
-                    s.score = Integer.parseInt(parse[1]);
-                    scores[i] = s;
-                    i++;
+                    while (i != 10)
+                    {
+                        scoreString[i] = reader.readLine();
+                        highScore s = new highScore();
+                        String parse[] = scoreString[i].split(" ");
+                        s.name = parse[0];
+                        s.score = Integer.parseInt(parse[1]);
+                        scores[i] = s;
+                        i++;
+                    }
                 }
+
             }
             catch(IOException e)
             {
 
             }
 
-        }
+    }
 
+    public void addScore(int score, String name)
+    {
+        String newString = "";
+        boolean newHighScore = false;
+        try
+        {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(ctx.openFileOutput(FILE, ctx.MODE_PRIVATE));
+            for(int i = 0; i < 10; i++)
+            {
+                if(score > scores[i].score)
+                {
+                    for(int j = 9; j > i; j--)
+                    {
+                        scores[j].score = scores[j-1].score;
+                        scores[j].name = scores[j-1].name;
+                    }
+                    scores[i].score = score;
+                    scores[i].name = name;
+                    newHighScore = true;
+                    break;
+                }
+            }
+            if(newHighScore)
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    newString = scores[i].name + " " + scores[i].score + "\n";
+                    scoreString[i] = newString;
+                    outputStreamWriter.write(scoreString[i]);
+                }
+            }
+            outputStreamWriter.close();
+        }
+        catch(IOException e) {
+
+        }
+    }
+
+    public String[] getScore()
+    {
+        return scoreString;
     }
 
 }
