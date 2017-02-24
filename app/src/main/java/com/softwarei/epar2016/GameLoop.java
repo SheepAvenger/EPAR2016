1d09d974586244d2853434bc54470232c44268ee
ccbt87 <ccbt87@gmail.com>
2017-02-24 10:33:14 -0800

    Created classes MainGame, GameView, GameLoop Renamed highScore to HighScore

---
+++ app/src/main/java/com/softwarei/epar2016/GameLoop.java
@@ -0,0 +1,5 @@
+package com.softwarei.epar2016;
+
+
+public class GameLoop extends Thread {
+}
---
+++ app/src/main/java/com/softwarei/epar2016/MainGame.java
@@ -0,0 +1,21 @@
+package com.softwarei.epar2016;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainGame extends Activity implements View.OnTouchListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        return true;
+    }
+}
+
