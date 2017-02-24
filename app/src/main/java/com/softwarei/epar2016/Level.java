0011d76d65a65f90625fd0951a5ca80a77964ad1
SheepAvenger <robert.mccook.elm@gmail.com>
2017-02-23 17:16:08 -0800

    Class Level

---
+++ app/src/main/java/com/softwarei/epar2016/Level.java
@@ -0,0 +1,46 @@
+package com.softwarei.epar2016;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rob on 2/23/2017.
+ */
+
+public class Level extends AppCompatActivity
+{
+    private int level;
+    private Bitmap Background[];
+    private Context ctx;
+
+    public Level(Context current)
+    {
+        level = 0;
+        Background = new Bitmap[10];
+        this.ctx = current;
+        int  x = R.drawable.l_0;
+        for(int i = 0; i < 10; i++)
+        {
+            x +=i;
+            Background[i] = BitmapFactory.decodeResource(ctx.getResources(), x);
+        }
+    }
+
+    public void setLevel()
+    {
+        level += 1;
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+
+    public Bitmap getBackground()
+    {
+        return Background[level];
+    }
+
+}
--- app/src/main/java/com/softwarei/epar2016/MainMenu.java
+++ app/src/main/java/com/softwarei/epar2016/MainMenu.java
@@ -10,17 +10,6 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         //2130837587
-        Bitmap t[] = new Bitmap[2];
-        int x;
-        for(int i = 0; i <= 1; i++)
-        {
-            x = R.drawable.l0;
-            x +=i;
-            t[i] = BitmapFactory.decodeResource(this.getResources(), x);
-            System.out.println(t[i]);
-
-        }
         setContentView(R.layout.activity_main_menu);
     }
 }
