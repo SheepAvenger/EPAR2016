121c496713f152d8cde37d668e6c6e113096b9e2
Michael <jones321michael@yahoo.com>
2017-02-26 01:25:38 -0800

    did some work to the vlad xml

---
+++ app/src/main/java/com/softwarei/epar2016/DealWithVlad.java
@@ -0,0 +1,10 @@
+package com.softwarei.epar2016;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Michael on 2/26/17.
+ */
+
+public class DealWithVlad extends AppCompatActivity{
+}
