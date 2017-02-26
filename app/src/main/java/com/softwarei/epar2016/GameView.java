4402899b0384ec3ef0d9dc9fc9a15ec18363231d
ccbt87 <ccbt87@gmail.com>
2017-02-26 09:48:15 -0800

    Added Sprite class, modified GameView class

--- app/src/main/java/com/softwarei/epar2016/GameView.java
+++ app/src/main/java/com/softwarei/epar2016/GameView.java
@@ -5,9 +5,16 @@ import android.content.Context;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import java.util.ArrayList;
-
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 480;
+    public static final int MOVESPEED = -5;
+
+    private long jumpButtonTime;
+    private GameLoop gameLoop;
+    private Background backGround;
+    private Sprite sprite;
 
     public GameView(Context context) {
         super(context);
---
+++ app/src/main/java/com/softwarei/epar2016/Sprite.java
@@ -0,0 +1,164 @@
+package com.softwarei.epar2016;
+
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+public class Sprite {
+    private int score;
+    private boolean jumping, falling, ducking, playing, collision;
+    private Animation animation = new Animation();
+    private Animation animation2 = new Animation();
+
+    private long startTime;
+    private int xInitial, xNext, xCurrent, y, width, height, xInitial2, xNext2, xCurrent2, y2, width2, height2;
+
+    public Sprite(Bitmap image, int numberOfFrames, Bitmap image2, int numberOfFrames2) {
+
+        int ground = -30;
+        score = 0;
+
+        this.height = image.getHeight();
+        this.width = image.getWidth()/numberOfFrames;
+        xInitial = 300;
+        xCurrent = xInitial;
+        y = GameView.HEIGHT - height + ground;
+        Bitmap[] playerImage = new Bitmap[numberOfFrames];
+        for(int i = 0; i < playerImage.length; i++) {
+            playerImage[i] = Bitmap.createBitmap(image, i*width, 0, width, height);
+        }
+        animation.setFrames(playerImage);
+        animation.setDelay(10);
+
+        this.height2 = image2.getHeight()/numberOfFrames2;
+        this.width2 = image2.getWidth();
+        xInitial2 = xCurrent + width - width2;
+        xCurrent2 = xInitial2;
+        y2 = GameView.HEIGHT - height2 + ground;
+        Bitmap[] playerImage2 = new Bitmap[numberOfFrames2];
+        for(int i = 0; i < playerImage2.length; i++) {
+            playerImage2[i] = Bitmap.createBitmap(image2, 0, i*height2, width2, height2);
+        }
+        animation2.setFrames(playerImage2);
+        animation2.setDelay(10);
+
+        startTime = System.nanoTime();
+    }
+
+    public void setJumping(long jumpButtonTime) {
+        if(!ducking) {
+            System.out.println("jumpButtonTime: " + jumpButtonTime / 1000000);
+            jumping = true;
+        }
+    }
+
+    public boolean getJumping() {
+        return jumping;
+    }
+
+    public boolean getFalling() {
+        return falling;
+    }
+
+    public void setDucking(boolean ducking) {
+        if(!jumping || !falling) {
+            this.ducking = ducking;
+        }
+    }
+
+    public void setCollision() {
+        collision = true;
+    }
+
+    public boolean getCollision() {
+        return collision;
+    }
+
+    public void update() {
+        long elapsed = (System.nanoTime() - startTime) / 1000000;
+        if(elapsed > 100) {
+            score++;
+            startTime = System.nanoTime();
+        }
+
+        if(ducking) {
+            animation2.update();
+        }
+        else {
+            animation.update();
+        }
+
+        if(jumping) {
+            y -= 10;
+        }
+        else if(y < 250) {
+            y += 10;
+        }
+        else {
+            falling = false;
+        }
+
+        if(y < 100) {
+            jumping = false;
+            falling = true;
+        }
+
+        if(collision) {
+            xCurrent += GameView.MOVESPEED;
+            xCurrent2 += GameView.MOVESPEED;
+            if(xCurrent <= xNext) {
+                xNext -= 50;
+                //collision = false;
+            }
+            if(xCurrent2 <= xNext2) {
+                xNext2 -= 50;
+                collision = false;
+            }
+        }
+
+    }
+
+    public void draw(Canvas canvas) {
+        if(ducking){
+            canvas.drawBitmap(animation2.getImage(), xCurrent2, y2, null);
+        }
+        else {
+            canvas.drawBitmap(animation.getImage(), xCurrent, y, null);
+        }
+    }
+
+    public int getScore() {
+        return (this.score * 3);
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean getPlaying() {
+        return this.playing;
+    }
+
+    public void setPlaying(boolean playing) {
+        xCurrent = xInitial;
+        xNext = xInitial - 50;
+        xCurrent2 = xInitial2;
+        xNext2 = xInitial2 - 50;
+        this.playing = playing;
+    }
+
+    public void resetScore() {
+        this.score = 0;
+    }
+
+    public Rect getRectangle() {
+        if(ducking){
+            return new Rect(xCurrent2, y2, xCurrent2+width2, y2+height2);
+        }
+        else {
+            return new Rect(xCurrent, y, xCurrent+width, y+height);
+        }
+
+    }
+}
