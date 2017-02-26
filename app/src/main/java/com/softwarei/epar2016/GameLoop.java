ef447ae4cf1e9ca095b08510e9abb98117709229
nzhang7 <nathanz789@gmail.com>
2017-02-26 15:30:18 -0800

    Update GameLoop.java

--- app/src/main/java/com/softwarei/epar2016/GameLoop.java
+++ app/src/main/java/com/softwarei/epar2016/GameLoop.java
@@ -1,5 +1,76 @@
 package com.softwarei.epar2016;
 
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
 
-public class GameLoop extends Thread {
+public class GameLoop extends Thread
+{
+    private int FPS = 60;
+    private double averageFPS;
+    private SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private boolean running;
+    public static Canvas canvas;
+
+    public GameLoop(SurfaceHolder surfaceHolder, GameView gameView)
+    {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+    }
+    @Override
+    public void run()
+    {
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 0;
+        int frameCount =0;
+        long targetTime = 1000/FPS;
+
+        while(running) {
+            startTime = System.nanoTime();
+            canvas = null;
+
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    this.gameView.update();
+                    this.gameView.draw(canvas);
+                }
+            } catch (Exception e) {
+            }
+            finally{
+                if(canvas!=null)
+                {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                    catch(Exception e){e.printStackTrace();}
+                }
+            }
+
+
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime-timeMillis;
+
+            try{
+                this.sleep(waitTime);
+            }catch(Exception e){}
+
+            totalTime += System.nanoTime()-startTime;
+            frameCount++;
+            if(frameCount == FPS)
+            {
+                averageFPS = 1000/((totalTime/frameCount)/1000000);
+                frameCount = 0;
+                totalTime = 0;
+                System.out.println(averageFPS);
+            }
+        }
+    }
+    public void setRunning(boolean b)
+    {
+        running = b;
+    }
 }
