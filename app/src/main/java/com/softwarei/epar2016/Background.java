1b19bbc5189fd278ef3fd5cf7a642245594c568a
nzhang7 <nathanz789@gmail.com>
2017-02-26 15:23:40 -0800

    Update Background.java

--- app/src/main/java/com/softwarei/epar2016/Background.java
+++ app/src/main/java/com/softwarei/epar2016/Background.java
@@ -1,5 +1,31 @@
 package com.softwarei.epar2016;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 public class Background {
+
+    private Bitmap image;
+    private int x, y, dx;
+
+    public Background(Bitmap res)
+    {
+        image = res;
+        dx = GameView.MOVESPEED;
+    }
+    public void update()
+    {
+        x+=dx;
+        if(x<-GameView.WIDTH){
+            x=0;
+        }
+    }
+    public void draw(Canvas canvas)
+    {
+        canvas.drawBitmap(image, x, y,null);
+        if(x<0)
+        {
+            canvas.drawBitmap(image, x + GameView.WIDTH, y, null);
+        }
+    }
 }
