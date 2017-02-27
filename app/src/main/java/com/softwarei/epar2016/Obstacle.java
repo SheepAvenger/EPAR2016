1080d069c818d055f3b4bdf0b1e7723b458028c2
nzhang7 <nathanz789@gmail.com>
2017-02-26 16:17:59 -0800

    Update Obstacle.java

--- app/src/main/java/com/softwarei/epar2016/Obstacle.java
+++ app/src/main/java/com/softwarei/epar2016/Obstacle.java
@@ -1,5 +1,34 @@
 package com.softwarei.epar2016;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 
 public class Obstacle {
+    private Bitmap image;
+    private int x, y, width, height;
+
+    public Obstacle(Bitmap img, int x, int y){
+        this.x = x;
+        this.y = y;
+        image = img;
+        height = image.getHeight();
+        width = image.getWidth();
+    }
+
+    public void update(){
+        x += GameView.MOVESPEED;
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(image, x, y, null);
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public Rect getRectangle(){
+        return new Rect(x, y, x + width, y + height);
+    }
 }
