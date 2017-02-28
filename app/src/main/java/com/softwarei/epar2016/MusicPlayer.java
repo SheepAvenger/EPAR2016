7656043e02c66957b3c06b186d489ce6a23789c5
SheepAvenger <robert.mccook.elm@gmail.com>
2017-02-28 14:19:10 -0800

    adding multisong music player

--- app/src/main/java/com/softwarei/epar2016/MusicPlayer.java
+++ app/src/main/java/com/softwarei/epar2016/MusicPlayer.java
@@ -1,9 +1,13 @@
 package com.softwarei.epar2016;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.os.IBinder;
 
 import java.util.List;
@@ -15,29 +19,66 @@ import static com.softwarei.epar2016.R.raw.music_main_menu;
  * Created by Rob on 2/25/2017.
  */
 
-public class MusicPlayer extends Service {
+public class MusicPlayer extends Service implements MediaPlayer.OnCompletionListener {
 
     private static final String TAG = null;
     MediaPlayer player;
+    MediaPlayer levels;
+    int[] songs = {R.raw.music_main_menu, R.raw.careless_whisper, R.raw.never_gonna_give_you_up, R.raw.pursite};
+    private int current_index = 1;
+
     public IBinder onBind(Intent arg0) {
 
         return null;
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        int[] songs = {R.raw.music_main_menu};
-        player = MediaPlayer.create(this,songs[0]);
-        player.setLooping(true); // Set looping
+        mainmenu();
+        //player.setLooping(true); // Set looping
 
-        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+    }
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        play(this);
+    }
 
-            public void onCompletion(MediaPlayer mp) {
-                mp.reset();
-                mp.start();
-            }
-        });
+    public void mainmenu()
+    {
+        player = MediaPlayer.create(getApplicationContext(),R.raw.bird);
+        player.setLooping(true);
 
+    }
+
+    public void play(Context ctx)
+    {
+        //player.stop();
+        levels = MediaPlayer.create(getApplicationContext(),songs[current_index]);
+        levels.setOnCompletionListener(this);
+        if(current_index == 3) // change to 10 when other songs added
+        {
+            current_index = 0;
+        }
+        current_index++;
+        AssetFileDescriptor afd = ctx.getResources().openRawResourceFd(songs[current_index]);
+        try
+        {
+            player.reset();
+            player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getDeclaredLength());
+            player.prepare();
+            player.start();
+        }
+        catch(IllegalArgumentException e)
+        {
+
+        }
+        catch (IllegalStateException e)
+        {
+        }
+        catch (IOException e)
+        {
+        }
     }
     public int onStartCommand(Intent intent, int flags, int startId) {
         player.start();
