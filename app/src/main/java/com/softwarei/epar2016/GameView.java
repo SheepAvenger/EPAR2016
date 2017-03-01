78c53462096e715c8c9a0adac94512e3b6529db8
ccbt87 <ccbt87@gmail.com>
2017-02-28 16:00:35 -0800

    Merge branch 'master' of D:\Documents\GitHub\EPAR2016 with conflicts.

--- app/src/main/java/com/softwarei/epar2016/GameView.java
+++ app/src/main/java/com/softwarei/epar2016/GameView.java
@@ -7,44 +7,54 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.graphics.BitmapFactory;
 
+import java.util.ArrayList;
+
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
-    public static final int MOVESPEED = -5;
-
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 480;
+    public static int MOVESPEED = -5;
+    int level;
+    private long gameStartTime;
     private long jumpButtonTime;
     private GameLoop gameLoop;
     private Background background;
     private Sprite sprite;
+    private ArrayList<Obstacle> obstacles;
     private int scandalCount;
 
-    private int screenWidth;
-    private int screenHeight;
-    
-    public GameView(Context context, int screenWidth, int screenHeight) {
+    public GameView(Context context) {
         super(context);
         getHolder().addCallback(this);
         setFocusable(true);
-        
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
-
+        gameStartTime = System.nanoTime();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
+        boolean retry = true;
+        int counter = 0;
+        while(retry && counter<1000) {
+            counter++;
+            try{gameLoop.setRunning(false);
+                gameLoop.join();
+                retry = false;
+                gameLoop = null;
 
+            }catch(InterruptedException e){e.printStackTrace();}
+
+        }
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder){
         background = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.level0));
         sprite = new Sprite(BitmapFactory.decodeResource(getResources(), R.drawable.c_washington_run), 2,
-                BitmapFactory.decodeResource(getResources(), R.drawable.c_washington_duck), 2,
-                screenWidth, screenHeight);
+                BitmapFactory.decodeResource(getResources(), R.drawable.c_washington_duck), 2);
         gameLoop = new GameLoop(getHolder(), this);
 
         gameLoop.setRunning(true);
@@ -89,16 +99,53 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         if (sprite.getPlaying()){
             background.update();
             sprite.update();
+
+            long timeElapsed = (System.nanoTime()-gameStartTime)/1000000;
+            if(timeElapsed >(2000 - sprite.getScore()/4)){
+                obstacles.add(new Obstacle(BitmapFactory.decodeResource(getResources(),R.drawable.o_donkey),WIDTH + 10, HEIGHT/2));
+                gameStartTime = System.nanoTime();
+            }
+
+            //loop through every missile and check collision and remove
+            for(int i = 0; i< obstacles.size(); i++)
+            {
+                //update missile
+                obstacles.get(i).update();
+
+                if(collision(obstacles.get(i),sprite))
+                {
+                    sprite.setCollision();
+
+                    obstacles.remove(i);
+                    scandalCount++;
+
+
+
+//                    break;
+                }
+                if(scandalCount >= 3 && !sprite.getCollision()) {
+                    sprite.setPlaying(false);
+                    scandalCount = 0;
+                    sprite.setRecovery();
+                }
+                //remove missile if it is way off the screen
+                if(obstacles.get(i).getX()<-100)
+                {
+                    obstacles.remove(i);
+                    break;
+                }
+            }
         }
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        
+        final float scaleFactorX = getWidth()/(WIDTH*1.f);
+        final float scaleFactorY = getHeight()/(HEIGHT*1.f);
         if (canvas != null) {
             final int savedState = canvas.save();
-            
+            canvas.scale(scaleFactorX, scaleFactorY);
             background.draw(canvas);
             sprite.draw(canvas);
 
