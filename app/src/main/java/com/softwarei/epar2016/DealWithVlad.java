3758ef8cdc1674b3964812822d8ee5c51e3ed92b
nzhang7 <nathanz789@gmail.com>
2017-03-02 22:37:00 -0800

    Reworked Vlad

--- app/src/main/java/com/softwarei/epar2016/DealWithVlad.java
+++ app/src/main/java/com/softwarei/epar2016/DealWithVlad.java
@@ -1,102 +1,136 @@
 package com.softwarei.epar2016;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
-
 /**
- * Created by Michael on 2/26/17.
+ * Created by nathan on 3/2/2017.
  */
 
-public class DealWithVlad extends AppCompatActivity{
-    private Bitmap slotMachine;
-    private double averageFPS;
-    private Bitmap slotScroll;
-    private Bitmap slotTrophy;
-    private Bitmap slotMoney;
-    private Bitmap slotGlory;
-    private Bitmap slotGameOver;
-    private Bitmap vlad[]; //need to add speech bubbles to the sprite vlad
-    private Sprite sprite;
-    public static Canvas canvas;
-    private SurfaceHolder surfaceHolder;
-    private int score;
-    private int FPS = 60;
+import android.graphics.Canvas;
+import android.content.Context;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 
+public class VladThread extends SurfaceView implements Runnable {
 
+    private volatile boolean running;
+    private Thread gameThread = null;
 
+    private Canvas canvas;
+    private SurfaceHolder ourHolder;
 
-    //character names from a txt file
+    Context context;
 
-    @Override
-    public void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.deal_with_vlad);
+    long fps = 60;
 
-        canvas = null;
-        canvas = new Canvas();
+    private int backWidth;
+    private int backHeight;
+    Bitmap backMap;
 
-        sprite = new Sprite(14, 10, 5, this);
+    private int vladWidth;
+    private int vladHeight;
+    Bitmap vladMap;
 
+    private int machineCols = 10;
+    private int machineSpriteFrame = 0;
+    private int machineWidth;
+    private int machineHeight;
+    Bitmap machMap;
 
-       // slotMachine = BitmapFactory.decodeResource(getResources(), R.drawable.slot_machine);
-        //slotScroll = BitmapFactory.decodeResource(getResources(), R.drawable.slot_scroll);
-        //slotTrophy = BitmapFactory.decodeResource(getResources(), R.drawable.slot_trophy_win);
-        //slotMoney = BitmapFactory.decodeResource(getResources(), R.drawable.slot_money_win);
-        //slotGlory = BitmapFactory.decodeResource(getResources(), R.drawable.slot_glory_win);
-        //slotGameOver = BitmapFactory.decodeResource(getResources(), R.drawable.slot_lose);
+    private int scrollCols = 5;
+    private int scrollSpriteFrame = 0;
+    private int scrollWidth;
+    private int scrollHeight;
+    Bitmap scrollMap;
 
-        //store new vlad images
+    VladThread(Context context) {
+        super(context);
+        this.context = context;
+        ourHolder = getHolder();
 
-        //score = Sprite.getScore();
+        backMap = BitmapFactory.decodeResource(getResources(), R.drawable.vlad_background);
+        this.backWidth = backMap.getWidth();
+        this.backHeight = backMap.getHeight();
+
+        vladMap = BitmapFactory.decodeResource(getResources(), R.drawable.vlad);
+        this.vladWidth = vladMap.getWidth();
+        this.vladHeight = vladMap.getHeight();
+
+        machMap = BitmapFactory.decodeResource(getResources(), R.drawable.slot_machine);
+        this.machineWidth = machMap.getWidth() / machineCols;
+        this.machineHeight = machMap.getHeight();
+
+        scrollMap = BitmapFactory.decodeResource(getResources(), R.drawable.slot_scroll);
+        this.scrollWidth = scrollMap.getWidth() / scrollCols;
+        this.scrollHeight = scrollMap.getHeight();
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void run() {
+        while (running) {
+            long startFrameTime = System.currentTimeMillis();
 
-        long startTime = 0;
-        long timeMillis;
-        long waitTime;
-        long totalTime = 0;
-        int frameCount = 0;
-        long targetTime = 1000 /FPS;
+            update();
+            draw();
 
-        while (true) {
-            this.sprite.update();
-            this.sprite.draw(canvas);
-
-            timeMillis = (System.nanoTime() - startTime) / 1000000;
-            waitTime = targetTime - timeMillis;
-
-            try {
-                Thread.sleep(waitTime);
-            } catch (Exception e) {
-            }
-
-            totalTime += System.nanoTime() - startTime;
-            frameCount++;
-            if (frameCount == FPS) {
-                averageFPS = 1000 / ((totalTime / frameCount) / 1000000);
-                frameCount = 0;
-                totalTime = 0;
-                //System.out.println(averageFPS);
+            long timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timeThisFrame >= 1) {
+                fps = 1000 / timeThisFrame;
             }
         }
-
-
     }
 
-    /*public void Power() {
+    private void update() {
+        if (running) {
+            machineSpriteFrame = ++machineSpriteFrame % machineCols;
+            scrollSpriteFrame = ++scrollSpriteFrame % scrollCols;
+        }
+    }
+    private void draw() {
 
+        if (ourHolder.getSurface().isValid()) {
+            canvas = ourHolder.lockCanvas();
+
+            final float scaleFactorX = getWidth()/(GameView.WIDTH*1.f);
+            final float scaleFactorY = getHeight()/(GameView.HEIGHT*1.f);
+            canvas.scale(scaleFactorX, scaleFactorY);
+
+            canvas.drawColor(Color.LTGRAY);
+
+            Rect bsrc = new Rect(0, 0, backWidth, backHeight);
+            Rect bdst = new Rect(0, 0, GameView.WIDTH, GameView.HEIGHT/2);
+            canvas.drawBitmap(backMap, bsrc, bdst, null);
+
+            Rect vsrc = new Rect(0, 0, vladWidth, vladHeight);
+            Rect vdst = new Rect(450, 40, 750, 240);
+            canvas.drawBitmap(vladMap, vsrc, vdst, null);
+
+            int msrcX = machineSpriteFrame * machineWidth;
+            Rect msrc = new Rect(msrcX, 0, msrcX + machineWidth, machineHeight);
+            Rect mdst = new Rect(50, 70, 350, 230);
+            canvas.drawBitmap(machMap, msrc, mdst, null);
+
+            int ssrcX = scrollSpriteFrame * scrollWidth;
+            Rect ssrc = new Rect(ssrcX, 0, ssrcX + scrollWidth, scrollHeight);
+            Rect sdst = new Rect(135, 125, 205, 175);
+            canvas.drawBitmap(scrollMap, ssrc, sdst, null);
+
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
     }
 
-    public void Wealth() {
-
+    public void pause() {
+        running = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {}
     }
-    public void Glory() {
 
-    }*/
+    public void resume() {
+        running = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 }
